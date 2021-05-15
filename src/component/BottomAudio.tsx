901fbd64f07ecce12bf7241689a125393b6f8e7a
Mohamed Habib Alouani <mhabib.alouani@gmail.com>
2021-05-15 13:06:32 +0000

    bottom Player

--- src/component/BottomAudio.tsx
+++ src/component/BottomAudio.tsx
@@ -1,11 +1,28 @@
+import { BlurView } from 'expo-blur';
+import EntypoIcon from "react-native-vector-icons/Entypo";
 import React,{FC} from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, Image, TouchableOpacity} from 'react-native';
 
 const BottomAudio:FC = () => {
   return(
     <>
       <View style={styles.container}>
-
+        <BlurView 
+          intensity={80}
+          style={{width: '100%', height: '100%',}}
+          tint="dark"
+        >
+          <View style={styles.myPlayer}>
+              <Image source={require('../../assets/emn.png')}  style={styles.musicImg} />
+            <View>
+              <Text style={styles.musicTitle} >Black Magic (feat. Sk....</Text>
+              <View style={{backgroundColor:'snow', height:2}}></View>
+            </View>
+            <TouchableOpacity>
+              <EntypoIcon name="controller-play" style={styles.icon}></EntypoIcon>
+            </TouchableOpacity>
+          </View>
+        </BlurView>
       </View>
     </>
   );
@@ -13,12 +30,19 @@ const BottomAudio:FC = () => {
 
 const styles = StyleSheet.create({
   container:{
-    backgroundColor: 'red',
+    backgroundColor: 'rgba(79, 79, 79, 0.2)',
     width: '100%',
     position: 'absolute',
     bottom: 0,
-    height: 130,
-  }
+    height: 135,
+  },
+  icon:{
+    color: 'snow',
+    fontSize: 60
+  },
+  myPlayer:{display: 'flex', justifyContent: 'space-around', flexDirection: 'row',alignItems: 'baseline'},
+  musicImg: {width: 60, height: 60, borderRadius: 10},
+  musicTitle: {color: 'snow', fontSize: 16, fontWeight: '500'},
 })
 
 export default BottomAudio
