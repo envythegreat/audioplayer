0206f34a032137b713bf2123ae895f9c5c1b46f4
Mohamed Habib Alouani <mhabib.alouani@gmail.com>
2021-06-10 23:37:20 +0100

    create a download api

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,7 +1,30 @@
 import {TRoutesInput} from '../types';
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+
+
+
 
 export default ({app}: TRoutesInput) => {
   app.get('/audio', async (req, res) => {
     return res.send({mydata: 'hello'})
   })
+
+  
+  app.post('/downloader', async (req, res) => {
+    const Url = req.body.VUrl
+    const name = req.body.VName
+    // { quality: 'highestaudio' }
+    let info = await ytdl.getInfo(Url);
+    ytdl(Url, { quality: 'highestaudio' })
+    .pipe(fs.createWriteStream(`./downloader/${name}.mp3`));
+    return res.send({info})
+  })
+
+
+  app.get('/audioUrl', async (req, res) => {
+    const name = req.query.VName
+    res.download(`./downloader/${name}.mp3`);
+  })
+
 }
